4dc4177c6f56be51f52b315eae1c26062d1db74d
leegaarden <dlwjddnjs081723@naver.com>
2025-01-11 21:38:12 +0900

    init : 초기 설정

---
+++ src/main/java/com/umc/yourun/YourunApplication.java
@@ -0,0 +1,13 @@
+package com.umc.yourun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourunApplication.class, args);
+    }
+
+}
