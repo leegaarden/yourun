c5bb8692adbd05d91b1d2e425a89e9425fdd02c5
leegaarden <dlwjddnjs081723@naver.com>
2025-01-12 14:13:41 +0900

    feat #2 : 응답 에러 형식 Record로 수정

--- src/main/java/com/umc/yourun/config/exception/ErrorResponse.java
+++ src/main/java/com/umc/yourun/config/exception/ErrorResponse.java
@@ -1,14 +1,4 @@
 package com.umc.yourun.config.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class ErrorResponse {
-
-    private final int code;
-    private final String message;
-
+public record ErrorResponse(int code, String message) {
 }
