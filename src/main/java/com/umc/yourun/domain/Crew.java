0755f6fd2f794f2d7ec7d3929f59dedd6589a92c
leegaarden <dlwjddnjs081723@naver.com>
2025-01-12 16:59:18 +0900

    feat #1 : 엔터티 생성 및 관계 주입

---
+++ src/main/java/com/umc/yourun/domain/Crew.java
@@ -0,0 +1,19 @@
+package com.umc.yourun.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Crew extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
--- src/main/java/com/umc/yourun/domain/challenge/Challenge.java
+++ src/main/java/com/umc/yourun/domain/challenge/Challenge.java
@@ -1,13 +1,55 @@
 package com.umc.yourun.domain.challenge;
 
 import com.umc.yourun.domain.BaseEntity;
+import com.umc.yourun.domain.enums.ChallengeDistance;
+import com.umc.yourun.domain.enums.ChallengeKind;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Challenge extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private ChallengeKind kind;
+
+    @Enumerated(EnumType.STRING)
+    private ChallengeDistance distance;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @OneToMany(mappedBy = "challenge")
+    private List<SoloChallenge> soloChallenges = new ArrayList<>();
+
+    @OneToMany(mappedBy = "challenge")
+    private List<CrewChallenge> crewChallenges = new ArrayList<>();
+
+    @Builder
+    public Challenge(String name, ChallengeKind kind, ChallengeDistance distance,
+                     LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.kind = kind;
+        this.distance = distance;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
 }
