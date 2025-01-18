abb27351ddbaa162ffc3ea5c663daa1dde210efa
Miging <dnfldpden32@gmail.com>
2025-01-18 18:09:49 +0900

    chore: dto class 로 수정

--- src/main/java/com/umc/yourun/dto/runningdata/RunningDataRequestDTO.java
+++ src/main/java/com/umc/yourun/dto/runningdata/RunningDataRequestDTO.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public class RunningDataRequestDTO() {
+public class RunningDataRequestDTO {
 	@Schema(title = "RUNNING_REQ_01 : 러닝 결과 정보 생성 요청")
 	public record CreateRunningDataReq(
 		@Schema(description = "시작 시간", example = "2025-01-17T15:00:00")
