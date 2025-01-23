e0eb32a93334380f38591d6e8facddcf3c9665b6
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2025-01-23 12:01:25 +0900

    fix 비밀번호 및 닉네임 형식 지정

--- src/main/java/com/umc/yourun/domain/User.java
+++ src/main/java/com/umc/yourun/domain/User.java
@@ -5,6 +5,7 @@ import com.umc.yourun.domain.enums.UserStatus;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
@@ -27,19 +28,22 @@ public class User extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Email
     @NotBlank
+    @Email(message = "올바른 형식의 이메일 주소여야 합니다.")
     @Column(length = 30, nullable = false, unique = true)
     private String email;
 
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,10}$", message = "비밀번호는 최소 8자 이상 10자 미만이고 영문, 숫자를 포함해야 합니다.")
     @Column(length = 100, nullable = false)
     private String password;
 
+    
+    @Pattern(regexp = "^[가-힣]{2,4}$", message = "닉네임은 띄어쓰기 없이 한클 2~4자만 가능합니다.")
     @Column(length = 10, nullable = false, unique = true)
     private String nickname;
 
     @Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "VARCHAR(10) DEFAULT 'ACTIVE'")
+    @Column(columnDefinition = "VARCHAR(10)")
     private Tendency tendency;
 
     private Long crewReward;
--- src/main/java/com/umc/yourun/domain/enums/Tendency.java
+++ src/main/java/com/umc/yourun/domain/enums/Tendency.java
@@ -1,4 +1,5 @@
 package com.umc.yourun.domain.enums;
 
 public enum Tendency {
+    페이스메이커, 스프린터, 트레일러너
 }
