2bd13f936929bb182199a88269290619416a5b82
Choi-seokun <142799427+Choi-seokun@users.noreply.github.com>
2025-01-31 14:11:01 +0900

    Feature 예외처리를 제외한메이트CRUD로직 구현

--- src/main/java/com/umc/yourun/domain/User.java
+++ src/main/java/com/umc/yourun/domain/User.java
@@ -62,11 +62,11 @@ public class User extends BaseEntity{
     private List<RunningData> runningData = new ArrayList<>();
 
     @OneToMany(mappedBy = "user")
-    private List<UserMateuserMate = new ArrayList<>();
+    private List<UserMate> userMates = new ArrayList<>();
 
     @OneToMany(mappedBy = "user")
     @Getter
-    private List<UserTag> userTag = new ArrayList<>();
+    private List<UserTag> userTags = new ArrayList<>();
 
     public void encodePassword(String password) {
         this.password = password;
--- src/main/java/com/umc/yourun/dto/user/UserResponseDTO.java
+++ src/main/java/com/umc/yourun/dto/user/UserResponseDTO.java
@@ -1,4 +1,25 @@
 package com.umc.yourun.dto.user;
 
+import com.umc.yourun.domain.enums.Tag;
+import com.umc.yourun.domain.enums.Tendency;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+
+import java.util.List;
+
 public class UserResponseDTO {
+
+    @Builder
+    public record userMateInfo(
+            Long id,
+            @Pattern(regexp = "^[가-힣]{2,4}$", message = "닉네임은 띄어쓰기 없이 한글 2~4자만 가능합니다.")
+            @Schema(example = "최석운")
+            String nickname,
+            @Schema(example = "페이스메이커")
+            Tendency tendency,
+            List<Tag> tags
+    ){}
 }
