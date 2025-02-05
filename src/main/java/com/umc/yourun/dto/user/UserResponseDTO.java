e84ca782394691639b64d0db9bc7696557299793
Miging <dnfldpden32@gmail.com>
2025-02-05 15:47:04 +0900

    feat: dto 구조 생성

--- src/main/java/com/umc/yourun/dto/user/UserResponseDTO.java
+++ src/main/java/com/umc/yourun/dto/user/UserResponseDTO.java
@@ -2,6 +2,7 @@ package com.umc.yourun.dto.user;
 
 import com.umc.yourun.domain.enums.Tag;
 import com.umc.yourun.domain.enums.Tendency;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -12,16 +13,36 @@ import java.util.List;
 
 public class UserResponseDTO {
 
-    @Builder
-    public record userMateInfo(
-            @Schema(example = "1")
-            Long id,
-            @Schema(example = "최석운")
-            String nickname,
-            @Schema(example = "페이스메이커")
-            Tendency tendency,
-            List<Tag> tags,
-            int totalDistance,
-            int countDay
-    ){}
+	@Builder
+	public record userMateInfo(
+		@Schema(example = "1")
+		Long id,
+		@Schema(example = "최석운")
+		String nickname,
+		@Schema(example = "페이스메이커")
+		Tendency tendency,
+		List<Tag> tags,
+		int totalDistance,
+		int countDay
+	) {
+	}
+
+	@Builder
+	public record userInfo(
+		@Schema(example = "1")
+		Long id,
+		@Schema(example = "최석운")
+		String nickname,
+		@Schema(example = "페이스메이커")
+		Tendency tendency,
+		@Schema(example = "#느긋하게,#에너자이저")
+		List<Tag> tags,
+		@Schema(example = "1")
+		Long crewReward,
+		@Schema(example = "1")
+		Long personalReward,
+		@Schema(example = "1")
+		Long mvp
+		) {
+	}
 }
