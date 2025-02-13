534822a49a7d7f5add2a99b4344717adc5315aea
leegaarden <dlwjddnjs081723@naver.com>
2025-02-14 02:32:29 +0900

    hotfix : 모니터링 에러 해결을 위한 관련 파일 수정

--- src/main/java/com/umc/yourun/config/SecurityConfig.java
+++ src/main/java/com/umc/yourun/config/SecurityConfig.java
@@ -1,53 +1,27 @@
 package com.umc.yourun.config;
 
-import com.umc.yourun.domain.User;
 import com.umc.yourun.service.CustomOAuth2UserService;
-import com.umc.yourun.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.security.Provider;
-import java.util.Collection;
-import java.util.List;
-import java.util.UUID;
-
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-    private CustomOAuth2UserService customOAuth2UserService;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Autowired
     public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
@@ -57,18 +31,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
-//                .securityMatcher("/users/**")
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/actuator/**", "/actuator/prometheus/**").permitAll()  // 더 구체적인 경로 추가
-                        .requestMatchers("api/v1/users/login", "api/v1/users", "api/v1/users/duplicate",
-                                "api/v1/users/check-nickname", "/swagger-ui/**", "/api-docs/**",
-                                "/api/v1/oauth2/**", "/login/oauth2/**", "/oauth2/**").permitAll()
-                        //                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(
+                                "/actuator/**",
+                                "/swagger-ui/**",
+                                "/api-docs/**",
+                                "/api/v1/users/login",
+                                "/api/v1/users",
+                                "/api/v1/users/duplicate",
+                                "/api/v1/users/check-nickname",
+                                "/api/v1/oauth2/**",
+                                "/login/oauth2/**",
+                                "/oauth2/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-                                             .defaultSuccessUrl("/api/v1/users/kakao-login", true)
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+                        .defaultSuccessUrl("/api/v1/users/kakao-login", true)
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(csrf -> csrf.disable());
@@ -77,39 +58,37 @@ public class SecurityConfig {
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*"); // 모든 Origin 허용
-        configuration.addAllowedMethod("*");       // 모든 HTTP 메서드 허용
-        configuration.addAllowedHeader("*");       // 모든 Header 허용
-        configuration.setAllowCredentials(false);   // 인증 정보 허용
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
-
-    @Bean
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-        // Spring Authorization Server 기본 설정 적용
-        //OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-
         http
-                .securityMatcher("/oauth/**") // /oauth/** 경로만 처리
+                .securityMatcher("/oauth/**")
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/oauth/token", "/actuator/**", "/actuator/prometheus/**").permitAll() // /oauth/token은 공개
-                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+                        .requestMatchers("/oauth/token", "/actuator/**").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/oauth/token")); // CSRF 비활성화
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/oauth/token", "/actuator/**")
+                );
 
         return http.build();
     }
 
     @Bean
-    public AuthorizationServerSettings authorizationServerSettings(@Value("${springdoc.security.oauth2.client.registration.custom-client.token-uri}") String tokenUri){
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings(@Value("${springdoc.security.oauth2.client.registration.custom-client.token-uri}") String tokenUri) {
         return AuthorizationServerSettings.builder()
-                .issuer(tokenUri) // 인증 서버의 기본 주소
+                .issuer(tokenUri)
                 .tokenEndpoint("/oauth/token")
                 .build();
     }
